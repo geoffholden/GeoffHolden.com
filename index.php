6ca8901c78ee320c13f22f4d67cf6ce447b5f3ce
Geoff Holden <gholden@ieee.org>
2008-04-10 13:57:14 +0000

    Added redirect for old ?content= crap that's still in Google.

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
+if ($_GET['content']) {
+	header('Location: http://www.geoffholden.com/',TRUE,301);
+	return;
+}
 /* Short and sweet */
 define('WP_USE_THEMES', true);
 require('./wordpress/wp-blog-header.php');
