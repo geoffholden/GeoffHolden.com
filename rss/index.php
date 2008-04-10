4b60a7e182da6439cdbb8d14c7283b746ef06632
Geoff Holden <gholden@ieee.org>
2008-04-10 13:56:26 +0000

    Fixed content-type and XML imports.

--- rss/index.php
+++ rss/index.php
@@ -1,5 +1,10 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<rss version="2.00">
+<?php
+header("Content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+
+<rss
+  version="2.0"
+  xmlns:content="http://purl.org/rss/1.0/modules/content/">
 <channel>
 <language>en-us</language> 
 
