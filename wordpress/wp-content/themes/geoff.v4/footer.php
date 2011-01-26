a045cbc84357f5d12a2a24dd2a6f9e84535bc261
Geoff Holden <gholden@ieee.org>
2011-01-26 11:26:09 -0330

    switched from wordpress to new landing page

--- wordpress/wp-content/themes/geoff.v4/footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-	<img src="<?php bloginfo('stylesheet_directory'); ?>/images/footer.png" alt="" width="800" height="50" />
-</div>
-</div>
-
-		<?php wp_footer(); ?>
-</body>
-</html>
